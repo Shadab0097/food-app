5b0d39bc55d485c0e7e4cb779e2562b42ba1f2db
shadab khan <shadabkhan2910@gmail.com>
2023-08-07 13:26:07 +0530

    13

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,7 +14,7 @@ const Cart =()=>{
     }
     return(
         <>
-        <div className="m-4 p-4  max-[480px]:ml-0 pl-1.5">
+        <div className="m-2 p-2  max-[480px]:ml-3 pl-1">
             <h1 className=" text-center font-bold text-2xl">cart</h1>
             <div className="w-7/12 m-auto max-[480px]:ml-0 ">
          {cartItems.length > 0 && <button className=" bg-black text-white rounded-lg p-1"  onClick={HandleClearCart}>Clear Cart</button> }   
